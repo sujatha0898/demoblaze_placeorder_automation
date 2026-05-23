14dfee0f988a7055f50a8278468ba46655d17771
sujatha0898 <sujatha.subramani98@gmail.com>
2026-05-23 23:11:24 +0530

    Remove unwanted test files, keep placeOrder.spec.js

--- tests/org.spec.js
+++
@@ -1,7 +0,0 @@
-import {test,expect} from '@playwright/test'
-import fixture2 from '../Pages/Fictures/fixture2'
-test.beforeAll('loginurl', async({page})=>{
-await page.goto(loginurl.loginurl)
-await page.waitForTimeout(5000);
-
-})
